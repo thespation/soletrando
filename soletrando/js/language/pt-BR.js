ab412be6ffb93e2fecf1d8cf42954350d65771f5
William Santos <thespation@gmail.com>
2026-09-24 16:35:52 -0300

    Delete soletrando directory

--- soletrando/js/language/pt-BR.js
+++
@@ -1,4 +0,0 @@
-// ===== Dicionário português (pt-BR) =====
-// As chaves são o texto original em português; este idioma não precisa de traduções.
-window.I18N_DICTS = window.I18N_DICTS || {};
-window.I18N_DICTS['pt-BR'] = {}
